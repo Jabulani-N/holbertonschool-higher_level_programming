caea2d8394bee57c73d2d4ca34172b911fc1cbed
Jabulani-N <114835664+Jabulani-N@users.noreply.github.com>
2023-05-11 01:57:43 +0000

    pep8 complinace

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-number = 3.14159
-# number is gotten from given source code
+number = 3.14159  # number is gotten from given source code
 print(f"Float: {number:.2f}")
--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[0:3]#word but only characters 0-3
-word_last_2 = word[-2:]#word, but only the 2nd from end charcter to end
-middle_word = word[1:-1]#word, but only the 1st from start to 1st from end
+word_first_3 = word[0:3]  # word but only characters 0-3
+word_last_2 = word[-2:]  # word, but only the 2nd from end charcter to end
+middle_word = word[1:-1]  # word, but only the 1st from start to 1st from end
 print(f"First 3 letters: {word_first_3}"), print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
