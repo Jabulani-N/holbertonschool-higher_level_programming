3b5d39d378e67d83c42b0acb21539ac4a6399f63
Jabulani-N <114835664+Jabulani-N@users.noreply.github.com>
2023-05-11 01:51:42 +0000

    more betty2.0 (pep8) compliance

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-new = '\n'
-print(f"{str}{str}{str}{new}{str[:9]}")
-#serially prints the strings specified.
-#print automatiaclly ends in a newline
+new = '\n'#below serially prints the strings specified.
+print(f"{str}{str}{str}{new}{str[:9]}")#print automatiaclly ends in a newline
--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-import this
-#it's a secret macro that gives a spiel
+import this#it's a secret macro that gives a spiel
