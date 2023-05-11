4247f5fd3cb016754cb72bc58d68149e98b0050c
Jabulani-N <114835664+Jabulani-N@users.noreply.github.com>
2023-05-11 14:30:07 -0500

    Create add_0.py
    so we can look back at this and understand what task 0 is grabbing and from where

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#  the example add_0.py for task 0 testing
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
