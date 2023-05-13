ac0323dffef73c293b7a42246a56bffb10ecc357
Jabulani-N <jabu.94@gmail.com>
2023-05-13 14:57:28 -0700

    made_non-list_handling_version-independant

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     # copied form task 0, but subtracting index from length to print backward
-    if type(my_list) != "built-in class list":  # type(me) returns the type of me
-        return None
+    if type(my_list) != type([1,2]):  # type(me) returns the type of me
+        return None # example array makes code prtable regardless of pyversion
     for index in reversed(my_list):  # reversed works on any sequence
         # creates variable "index" to loop through reverse of "my_list"
         print("{:d}".format(index))
