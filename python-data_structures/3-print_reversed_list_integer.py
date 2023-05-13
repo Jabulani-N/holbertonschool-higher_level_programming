b13550d99130fae7b4a381903b43449c47f67835
Jabulani-N <jabu.94@gmail.com>
2023-05-13 15:03:14 -0700

    snake-betty-compliance-2

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,7 @@ def print_reversed_list_integer(my_list=[]):
     # copied form task 0, but subtracting index from length to print backward
     # type(me) returns the type of me
     # isinstance(me, extype) returns true if type of me is extype
-    if isinstance(my_list,list):  # list by itself returns [] 
+    if isinstance(my_list, list):  # list by itself returns []
         return None  # example array makes code prtable regardless of pyversion
     for index in reversed(my_list):  # reversed works on any sequence
         # creates variable "index" to loop through reverse of "my_list"
