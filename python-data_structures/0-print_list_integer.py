69922452e99c5ef335db7da7775ab150622bd2e9
Jabulani-N <jabu.94@gmail.com>
2023-05-12 20:23:14 -0700

    added tutorial and proper formatting

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     #  I believe the above is the prototype of the function
-    for item in my_list:
-        print(my_list[item])
+    str = "{content}" #  brackets mean formattable variable
+    for index in my_list: #  creates variable "index" to loop through "my_list"
+        #loops through my_list. everything indented gets looped
+        print(str.format(content = index))
+        #  str.format(variable = anything_I_want)
+#  no indent = no loop
---
+++ python-data_structures/examples/for_loop_python.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  print("me too")
+print("me only once please")
+  
