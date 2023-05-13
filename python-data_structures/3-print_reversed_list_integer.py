88b912193e221373b83af2361e0e1cc8c60a33e7
Jabulani-N <jabu.94@gmail.com>
2023-05-13 14:48:00 -0700

    handles_empty_inputs

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     # copied form task 0, but subtracting index from length to print backward
+    if len(my_list) == 0:
+        return None
     for index in reversed(my_list):  # reversed works on any sequence
         # creates variable "index" to loop through reverse of "my_list"
         print("{:d}".format(index))
