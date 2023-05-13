710a9c51da075ec06a0e77c50cf30b7a63238370
Jabulani-N <jabu.94@gmail.com>
2023-05-12 20:40:35 -0700

    corrected_misunderstanding_of_requested_approach.

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     # I believe the above is the prototype of the function
-    str = "{content}"  # brackets mean formattable variable
     for index in my_list:  # creates variable "index" to loop through "my_list"
         # loops through my_list. everything indented gets looped
-        print(str.format(content=index))
-        # str.format(variable = anything_I_want)
+        print("{:d}".format(index))
 # no indent = no loop
--- python-data_structures/README.md
+++ python-data_structures/README.md
@@ -6,9 +6,17 @@ receive an array of integers. iterate through it and print it.
 
 - printing must utilize "str.format"
 
+- - that means it must look like `print("{:d}".format(index))`
+
+it does not want the approach below.
+
 `print(x.format)` is the same as `print(x)`, but it lets you alter the contents of x for the print.
 
+
 `
+
 x = "look at my beautiful {y}"
+
 print ("x says" + x.format(y="content"))
+
 `
