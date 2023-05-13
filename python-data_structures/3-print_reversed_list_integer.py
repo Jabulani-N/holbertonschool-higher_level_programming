759536c83f6e087173cefd44c1496fe99a145ab9
Jabulani-N <jabu.94@gmail.com>
2023-05-13 14:28:48 -0700

    typo_correcction

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def def print_reversed_list_integer(my_list=[]):
+def print_reversed_list_integer(my_list=[]):
     # copied form task 0, but subtracting index from length to print backward
     for index in my_list:  # creates variable "index" to loop through "my_list"
         # loops through my_list. everything indented gets looped
