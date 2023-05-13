3f8b0fcd77a22a208b1fe65303f61665a9b0d441
Jabulani-N <jabu.94@gmail.com>
2023-05-13 15:27:12 -0700

    created_task4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3#!/usr/bin/python3
+# largely copied from task2
 def new_in_list(my_list, idx, element):
-    
+    if isinstance(my_list, list) is False:
+        return None
+    dupe_list = my_list.copy
+    if idx < 0 or idx >= len(my_list):
+        return dupe_list
+    else:
+        dupe_list[idx] = element
+        return dupe_list
