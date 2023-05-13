d28dc388feaba9f81a26d96bc134d2bbc049ca63
Jabulani-N <jabu.94@gmail.com>
2023-05-13 14:54:24 -0700

    corrected_non-list_handling

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     # copied form task 0, but subtracting index from length to print backward
-    if type(my_list) != "list":  # type(me) returns the type of me
+    if type(my_list) != "built-in class list":  # type(me) returns the type of me
         return None
     for index in reversed(my_list):  # reversed works on any sequence
         # creates variable "index" to loop through reverse of "my_list"
