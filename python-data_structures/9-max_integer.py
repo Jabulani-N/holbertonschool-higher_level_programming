561f0a701a576bce939771ac27e4583e67b8da39
Jabulani-N <jabu.94@gmail.com>
2023-05-13 17:39:41 -0700

    created_task9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == [] or isinstance(my_list, list) is False:
+        return None
+    maxi = my_list[0]
+    for content in my_list:
+        if content > maxi:
+            maxi = content
+    return maxi
+    
