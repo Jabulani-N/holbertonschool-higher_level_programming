0d7779787178cbdd84af387a94440fb458360d19
Jabulani-N <jabu.94@gmail.com>
2023-05-13 17:49:25 -0700

    fixed_copy_operation

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     if my_list == [] or isinstance(my_list, list) is False:
         return None
-    noob = my_list.copy
+    noob = my_list.copy()
     pos = 0
     for content in my_list:
         if content % 2 == 0:
