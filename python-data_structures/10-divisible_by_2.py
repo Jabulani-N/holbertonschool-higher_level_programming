6c0bf719b03482ee0924d6fd1f885692b9b7218e
Jabulani-N <jabu.94@gmail.com>
2023-05-13 17:48:31 -0700

    task10_created

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list == [] or isinstance(my_list, list) is False:
+        return None
+    noob = my_list.copy
+    pos = 0
+    for content in my_list:
+        if content % 2 == 0:
+            noob[pos] = True
+        else:
+            noob[pos] = False
+        pos += 1
+    return noob
