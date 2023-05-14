f74a7b7787328509b4abbd5310023938bf00a441
Jabulani-N <jabu.94@gmail.com>
2023-05-13 17:05:14 -0700

    task6_done_via_nested_for_loop_through_nested_list.-demonstrates_format-end

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if isinstance(matrix, list):
+        for dim1 in matrix:  # creates variable "index" to loop through "my_list"
+            dim1_size = len(dim1)
+            pos = 0
+            for index1 in dim1:
+                if pos >= dim1_size - 1:
+                    print("{:d}".format(index1))
+                else:
+                    print("{:d}".format(index1), end=' ')
+                pos += 1
