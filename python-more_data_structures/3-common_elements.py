9c4aae5f2ac9e1c2347f0610e5800782b17a1ff4
Jabulani-N <jabu.94@gmail.com>
2023-05-15 20:42:15 -0700

    removed_all_type-checking

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,9 +2,6 @@
 
 
 def common_elements(set_1, set_2):
-    if isinstance(set_1, dict) is False or isinstance(set_2, dict) is False:
-        return None
-
 
     commoners =  {item for item in set_1 if item in set_2}
     return commoners
