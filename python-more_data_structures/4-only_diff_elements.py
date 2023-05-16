561318b6a12a6a4ee23e797d7b966ac783bc348e
Jabulani-N <jabu.94@gmail.com>
2023-05-15 21:12:50 -0700

    task4_handles_one_set_being_larger

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,5 +3,7 @@
 
 def only_diff_elements(set_1, set_2):
 
-    commoners = {item for item in set_1 if not item in set_2}
+    commoners = {item for item in set_1 not in item in set_2}
+    commoners += {item for item in set_2 not in item in set_1}
+ # done in doubles in case one is larger
     return commoners
