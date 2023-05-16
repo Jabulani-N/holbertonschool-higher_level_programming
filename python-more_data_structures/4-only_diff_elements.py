1d71ade14c3d785f864d1ea9dce4e689bf3cba08
Jabulani-N <114835664+Jabulani-N@users.noreply.github.com>
2023-05-15 23:41:03 -0500

    task4: removed conditionals so ti adds uniques from boths sets

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 
 
 def only_diff_elements(set_1, set_2):
-    if len(set_1) >= len(set_2):
-        commoners = {item for item in set_1 if item not in set_2}
-    else:
-        commoners = {item for item in set_2 if item not in set_1}
-# done in doubles in case one is larger
+
+    commoners = {item for item in set_1 if item not in set_2}
+
+    commoners = {item for item in set_2 if item not in set_1}
+# done in doubles to collect uniques form both sets
     return commoners
