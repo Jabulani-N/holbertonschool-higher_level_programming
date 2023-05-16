7919ba5c5367c5841000ce19b2a7e34d77c4083f
Jabulani-N <jabu.94@gmail.com>
2023-05-15 21:14:24 -0700

    fixed_for_loop

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -4,6 +4,6 @@
 def only_diff_elements(set_1, set_2):
 
     commoners = {item for item in set_1 not in item in set_2}
-    commoners += {item for item in set_2 not in item in set_1}
+    commoners += {item2 for item2 in set_2 not in item2 in set_1}
  # done in doubles in case one is larger
     return commoners
