b140287f0b048805a881fa982beace5c8dbe9452
Jabulani-N <jabu.94@gmail.com>
2023-05-16 15:53:27 -0700

    better_realized_trask6-demonstrates_sorted

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,7 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    print(sorted(a_dictionary))
+    for item in sorted(a_dictionary):
+        print(item, a_dictionary[item], sep=': ')
+# sorted returns a list (not dict) of the keys in the provided dict
+# sorted also sorts lists
