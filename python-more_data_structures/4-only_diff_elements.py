0ca0c4d7dd36cd82f6db02765414074728b8fc1d
Jabulani-N <114835664+Jabulani-N@users.noreply.github.com>
2023-05-16 00:01:55 -0500

    task4 updated to use update

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -5,6 +5,6 @@ def only_diff_elements(set_1, set_2):
 
     commoners = {item for item in set_1 if item not in set_2}
 
-    commoners = {item for item in set_2 if item not in set_1}
+    commoners.update({item for item in set_2 if item not in set_1})
 # done in doubles to collect uniques form both sets
     return commoners
