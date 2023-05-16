1577cf15ae98855c80a43f73beba990beec4d494
Jabulani-N <jabu.94@gmail.com>
2023-05-15 20:19:48 -0700

    bugfix

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -10,6 +10,6 @@ def common_elements(set_1, set_2):
     commoners = []
     for item1 in set_1:
         for item2 in set_2:
-            if item == item2:
+            if item1 == item2:
                 commoners.append(item1)
     return commoners
