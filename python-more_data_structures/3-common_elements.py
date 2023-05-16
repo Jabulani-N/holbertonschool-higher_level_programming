76c1295492e223054c662239e823f504247b0727
Jabulani-N <jabu.94@gmail.com>
2023-05-15 20:37:18 -0700

    syntax_repair

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -7,4 +7,5 @@ def common_elements(set_1, set_2):
     if set_1 == {} or set_2 == {}:
         return {}
 
-return {for item in set_1 if item in set_2}
+    commoners =  {item for item in set_1 if item in set_2}
+    return commoners
