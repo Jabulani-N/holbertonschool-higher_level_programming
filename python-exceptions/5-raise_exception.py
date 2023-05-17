19416128736c2bacb47de4d807b3d0fa2bfbf5af
Jabulani-N <jabu.94@gmail.com>
2023-05-16 18:01:47 -0700

    made_possible_by_contributions_of_others

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError
+# copied with gratitude from another person's repository
---
+++ python-exceptions/README.md
@@ -0,0 +1,3 @@
+# python-exceptions
+
+when things go wrong, handling exceptions let us keep on going.
