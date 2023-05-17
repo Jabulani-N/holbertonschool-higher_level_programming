f8cfc693488090487845c17dee35af044bb59884
Jabulani-N <jabu.94@gmail.com>
2023-05-16 17:02:42 -0700

    handles_empty_dictionaries

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 
 def best_score(a_dictionary):
     bestgirl = 'None'
-    if isinstance(a_dictionary, dict):
+    if isinstance(a_dictionary, dict) and len(a_dictionary) > 0:
         charmpoints = max(a_dictionary.values())
         for key in a_dictionary:
             if a_dictionary[key] == charmpoints:
