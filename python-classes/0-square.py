7607282711cdbf5fe4a0104aa03a90bd34751034
Jabulani-N <jabu.94@gmail.com>
2023-05-18 16:49:49 -0700

    added_module_documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+"""module documentation
 
+this module, 0-square.py, crates a class: square.
+"""
 
 class square:
     """'Object-class summary documentation
-    sttributes will be documented in the below section
+
+    attributes will be documented in the below section
     indent them like anything else protected by a colon.
+
     Attributes:
     """
     def __init__(self):
         """__init__ method documentation
+
         __init__ can optionally instead be documented in the class section
         do not include 'self' as an arg
 
