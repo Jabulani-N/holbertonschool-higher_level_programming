6465ac59472c5a632a9f8170f39504876d385c58
Jabulani-N <jabu.94@gmail.com>
2023-05-18 16:58:30 -0700

    removed___init___from_file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -12,12 +12,4 @@ class square:
 
     Attributes:
     """
-    def __init__(self):
-        """__init__ method documentation
-
-        __init__ can optionally instead be documented in the class section
-        do not include 'self' as an arg
-
-        'Args:' would go here
-        """
-        pass
+    pass
