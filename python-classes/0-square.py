27538d803fb54fb06d9404094bf4b1c20683ddf2
Jabulani-N <jabu.94@gmail.com>
2023-05-18 16:54:21 -0700

    reverted

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -10,11 +10,14 @@ class square:
     attributes will be documented in the below section
     indent them like anything else protected by a colon.
 
-
-    __init__ can optionally instead be documented like a method
-    do not include 'self' as an arg
     Attributes:
     """
     def __init__(self):
+        """__init__ method documentation
 
+        __init__ can optionally instead be documented in the class section
+        do not include 'self' as an arg
+
+        'Args:' would go here
+        """
         pass
