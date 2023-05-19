693df97e2749c352bf64978eddca9e1ff9387aff
Jabulani-N <jabu.94@gmail.com>
2023-05-18 17:06:00 -0700

    pycodestyle_compliance-README_updated_with_pitfalls

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,15 +2,18 @@
 """module documentation
 
 this module, 0-square.py, crates a class: square.
+
 """
 
 class Square:
     """Object-class summary documentation.
 
+
     attributes will be documented in the below section
     indent them like anything else protected by a colon.
 
     Attributes:
+    
     """
     pass
     def __init__(self):
--- python-classes/README.md
+++ python-classes/README.md
@@ -1,3 +1,15 @@
+
 # Python - Classes and Objects
 
+
 a pyhton directory demonstrating class and object creation and usage
+
+
+# Task 0
+
+
+## Potential Pitfalls
+
+`B`e sure to capitalize your class's name.
+
+you will have failed to document "Classname" if you instead docuemnted "classname".
