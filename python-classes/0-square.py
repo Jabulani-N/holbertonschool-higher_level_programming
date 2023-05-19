45c94d684b34cc7e06cbca32b10e7770a3ce8835
Jabulani-N <jabu.94@gmail.com>
2023-05-18 17:00:26 -0700

    capitalized_Square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,7 +4,7 @@
 this module, 0-square.py, crates a class: square.
 """
 
-class square:
+class Square:
     """Object-class summary documentation.
 
     attributes will be documented in the below section
@@ -13,3 +13,12 @@ class square:
     Attributes:
     """
     pass
+    def __init__(self):
+        """__init__ method documentation
+
+        __init__ can optionally instead be documented in the class section
+        do not include 'self' as an arg
+
+        'Args:' would go here
+        """
+        pass
