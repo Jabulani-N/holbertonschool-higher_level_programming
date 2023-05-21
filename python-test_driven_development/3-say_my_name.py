d589d1586c482b26be081b4157b8332d592761df
Jabulani-N <jabu.94@gmail.com>
2023-05-21 02:17:43 -0700

    coding style fixes

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,7 @@ this module prints a sentance based on input argument stinrg
 requires a string input, or an error will be thrown
 """
 
+
 def say_my_name(first_name, last_name=""):
     if isinstance(first_name, str) is False:
         raise TypeError("first_name must be a string")
