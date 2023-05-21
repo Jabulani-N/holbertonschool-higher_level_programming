d61c74e05ceec0fbdb04bc5933daca64da9587ec
Jabulani-N <jabu.94@gmail.com>
2023-05-20 23:14:24 -0700

    task1_created

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""moldule 2-matrix_divided
+
+there is no module 1-...
+
+this module divides all elements of a matrix matrix by div
+"""
+
+
+def matrix_divided(matrix, div):
+    if isinstance(matrix, list) is False:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for checker in matrix:
+        if isinstance(checker, list) is False:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for item in checker:
+            if isinstance(item, int) is False and \
+               isinstance(item,float) is False:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            # all matrix/div instances below this point are valid
--- python-test_driven_development/README.md
+++ python-test_driven_development/README.md
@@ -7,4 +7,14 @@ This project demonstrates the use of doctest for teting python code
 
 ## Task 0
 
+### Known Issues
 
+is expecting error when no b value is given. should use the default value
+
+potnetial solution:
+
+- ensure the protootype matches request.
+
+- remove any checks for `b is None`
+
+- in **test** text file, ensure it expects tests with an empty b section to run normally
