a732d0fe133f343cec7a58dc6b805f89bd37a7eb
Jabulani-N <jabu.94@gmail.com>
2023-05-21 01:16:11 -0700

    task1 no longer alters oroginal matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,18 +9,25 @@ this module divides all elements of a matrix matrix by div
 
 def matrix_divided(matrix, div):
     if isinstance(matrix, list) is False:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) \
+                        of integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     for checker in matrix:
         if isinstance(checker, list) is False:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix \
+                            (list of lists) of integers/floats")
         for item in checker:
             if isinstance(item, int) is False and \
-               isinstance(item,float) is False:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+               isinstance(item, float) is False:
+                raise TypeError("matrix must be a matrix \
+                                (list of lists) of integers/floats")
             # all matrix/div instances below this point are valid
-    for checker in matrix:
-        for item in checker:
-            item = item/div
-    return matrix
+            divided_matrix = []
+            item_copy = []
+    for row in matrix:
+        for column in row:
+            item_copy.append(column/div)
+        divided_matrix.append(item_copy)
+
+    return divided_matrix
