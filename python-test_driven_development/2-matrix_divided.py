b7a57ae72e7413b1a673d638954df5e1e02aa2af
Jabulani-N <jabu.94@gmail.com>
2023-05-21 01:29:45 -0700

    fixed_decimal_specificity

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,7 +27,7 @@ def matrix_divided(matrix, div):
             item_copy = []
     for row in matrix:
         for column in row:
-            item_copy.append(format(column/div, f"{2}f"))
+            item_copy.append(round(column/div, 2))
         divided_matrix.append(item_copy)
 
     return divided_matrix
