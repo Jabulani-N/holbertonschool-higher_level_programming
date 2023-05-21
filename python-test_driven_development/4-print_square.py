4b49c739eaec0d4231a0c7de67532f9bf76dbf4d
Jabulani-N <jabu.94@gmail.com>
2023-05-21 02:56:53 -0700

    task 3 created. task 3 test file created.

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+
+
+def print_square(size):
+    """prints a square of #s based on size
+
+    largely copied from the test python classes directory
+    """
+    if isinstance(size, int) is False:
+        raise TypeError("size must be an integer")
+    if size < 0 and isinstance(size, float):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        print("")
+    else:
+        for row in range(size):
+            for column in range(size):
+                print("#", end='')
+            print("")
