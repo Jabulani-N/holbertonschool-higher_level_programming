1c7621cae6a949aabf0846810e69a53669c01484
Jabulani-N <jabu.94@gmail.com>
2023-05-21 20:28:42 -0700

    style_improvements

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,9 +7,9 @@ import unittest
 
 max_integer = __import__('6-max_integer').max_integer
 
+
 class testingTask6(unittest.TestCase):
     """tests a number of potential inputs to the provided file"""
-
 
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
@@ -18,13 +18,20 @@ class testingTask6(unittest.TestCase):
         self.assertEqual(max_integer([3]), 3)
 
     def test_len3_list(self):
-        self.assertEqual(max_integer([3,65,4]), 65)
+        self.assertEqual(max_integer([3, 65, 4]), 65)
 
     def test_with_negatives(self):
-        self.assertEqual(max_integer([-3,-65,-4]), -3)
+        self.assertEqual(max_integer([-3, -65, -4]), -3)
 
-    def test_with_non_int(self):
-        self.assertEqual(max_integer([-3,"hello",-4]), TypeError)
+    def test_ending_max(self):
+        self.assertEqual(max_integer([-3, -9999, -1]), -1)
+
+    def test_middle_max(self):
+        self.assertEqual(max_integer([-3, 9999, -1]), 9999)
+
+    def test_two_max(self):
+        self.assertEqual(max_integer([-3, 1, 1]), 1)
+
 
 if __name__ == '__main__':
     unittest.main
