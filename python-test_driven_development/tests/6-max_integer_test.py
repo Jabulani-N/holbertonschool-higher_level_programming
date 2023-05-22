4b280b614ac0cd3d1ae146c8874b9301afbeef5b
Jabulani-N <jabu.94@gmail.com>
2023-05-21 20:19:02 -0700

    test case: 0 length list created

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,8 +5,16 @@
 
 import unittest
 
+max_integer = __import__('6-max_integer').max_integer
+
 class testingTask6(unittest.TestCase):
     """tests a number of potential inputs to the provided file"""
+
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), 0)
+
+
 
 if __name__ == '__main__':
     unittest.main
