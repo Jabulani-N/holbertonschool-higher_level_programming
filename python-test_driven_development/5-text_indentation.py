eca7912e4f8812fcca3330b12011d5c9ed2156de
Jabulani-N <jabu.94@gmail.com>
2023-05-21 18:38:46 -0700

    updated_text_replacement_method

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,19 +9,16 @@ raises error on non-string
 
 
 def text_indentation(text):
-    """translates in a newline after each '. ', so it's '.\n' instead
+    """replaces in a newline after each '. ', so it's '.\n' instead
     same with ? and :
     """
     if isinstance(text, str) is False:
         raise TypeError("text must be a string")
     hold_string = text
-    dots = str.maketrans(". ",".\n\n")
-    quests = str.maketrans("? ","?\n\n")
-    cols = str.maketrans(": ",":\n\n")
-    hold_string = hold_string.translate(dots)
-    hold_string = hold_string.translate(quests)
-    hold_string = hold_string.translate(cols)
-    # translates all except the last character in the document
+    hold_string = hold_string.replace(". ", ".\n\n")
+    hold_string = hold_string.replace("? ", "?\n\n")
+    hold_string = hold_string.replace(": ", ":\n\n"cols)
+    # replaces all except the last character in the document
     # won't work on last char because it won't have trailing spce
     # also does not translate line-ending delims
     print(hold_string)
