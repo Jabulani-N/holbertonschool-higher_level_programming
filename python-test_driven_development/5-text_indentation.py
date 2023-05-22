95752c4060c4482ef641f35dfb60c73ef911020f
Jabulani-N <jabu.94@gmail.com>
2023-05-21 18:19:08 -0700

    updated task4 coding style and comments

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,15 +7,16 @@ only accepts string
 raises error on non-string
 """
 
+
 def text_indentation(text):
     """translates in a newline after each '. ', so it's '.\n' instead
     same with ? and :
     """
     if isinstance(text, str) is False:
-        raise TypeError ("text must be a string")
+        raise TypeError("text must be a string")
     holdstring = text
     hold_string.translate({ord('. '): '.\n\n'})
     hold_string.translate({ord('? '): '?\n\n'})
     hold_string.translate({ord(': '): ':\n\n'})
     # translates all except the last one in the document
-    # also doe snot translate line-ending delims
+    # also does not translate line-ending delims
