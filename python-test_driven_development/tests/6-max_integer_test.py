bc996c6cbf2a7a80f7b6f963bc2c6418eb7d77d7
Jabulani-N <jabu.94@gmail.com>
2023-05-21 20:23:55 -0700

    more_test_cases_added

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -14,7 +14,17 @@ class testingTask6(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
+    def test_len1_list(self):
+        self.assertEqual(max_integer([3]), 3)
 
+    def test_len3_list(self):
+        self.assertEqual(max_integer([3,65,4]), 65)
+
+    def test_with_negatives(self):
+        self.assertEqual(max_integer([-3,-65,-4]), -3)
+
+    def test_with_non_int(self):
+        self.assertEqual(max_integer([-3,"hello",-4]), TypeError)
 
 if __name__ == '__main__':
     unittest.main
