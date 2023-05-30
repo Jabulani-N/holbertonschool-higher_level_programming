fd8985a901ae2c41845bdaf21dcc1baa4b6b841d
Jabulani-N <jabu.94@gmail.com>
2023-05-30 15:27:52 -0700

    task4_created

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module returns Pyton object representation of a JSON string/file object
+requires importing json
+"""
+import json
+
+
+def from_json_string(my_str):
+    """uses load to return JSON of a string"""
+    return json.load(my_obj)
