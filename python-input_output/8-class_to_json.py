06d768df238ec0e4e500aa68f8836794e6ea1b4e
Jabulani-N <jabu.94@gmail.com>
2023-05-30 16:22:51 -0700

    updated_task8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""module returns JSON representation of a class's dict
-requires importing json
+"""module returns a class's dict in the form of a simple data structure
+the data structure used here is list
+requires no import
 """
-import json
 
 
 def class_to_json(obj):
-    """uses dumps to return JSON of a string
-    likely  works one some other objects too
-    may be able to use string() to turn the dict into a string to dump
-    """
-# need to seralize the __dict__
-    return json.dumps(obj.__dict__)
+    """returns list of obj's dict"""
+    return list(obj.__dict__)
