7afb46acc61be3a60205bc19f2959fa7245c6bd1
Jabulani-N <jabu.94@gmail.com>
2023-05-29 20:13:51 -0700

    updated_multiple_tasks

--- python-inheritance/2-is_same_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""module holds a function to tell if object is of class"""
-
-
-def is_same_class(obj, a_class):
-    """uses type, as isinstance allows inherited to be True"""
-    a = type(obj)
-    b = type(a_class)
-    return b == a
