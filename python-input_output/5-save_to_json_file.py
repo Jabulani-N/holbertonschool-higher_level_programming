9312eb27cdbf0cc6f9e9646f7194663548f0b4bf
Jabulani-N <jabu.94@gmail.com>
2023-05-30 15:48:35 -0700

    updated_task5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
 
     uses operations from file-writing
     """
-
-    json.dump(my_obj, filename)
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, filename)  # dump will do the writing
