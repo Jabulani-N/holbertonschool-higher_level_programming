3ba23ebea6c1e412b9d57f927c7236fcd904c02c
Jabulani-N <jabu.94@gmail.com>
2023-05-30 15:04:22 -0700

    task3_created

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module returns JSON representation of a string"""
+
+
+def to_json_string(my_obj):
+    """uses dumps to return JSON of a string
+    likely  works one some other objects too
+    """
+
+    return json.dumps(my_obj)
