ab470dc35142b36524fb920038b516da43722ed8
Jabulani-N <jabu.94@gmail.com>
2023-05-30 14:39:43 -0700

    updated readme

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module will append text to a file"""
+
+
+def append_write(filename="", text=""):
+    """appends text text to file filename
+    uses with operator
+    filename is the input argument
+    text is input argument
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        typed = f.append(text)
+    return typed
