c5f7eb764f79ea9dd909822924e9f02f98dd8acf
Jabulani-N <jabu.94@gmail.com>
2023-05-30 14:25:57 -0700

    updated readme

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module will read a file and print it"""
+
+
+def read_file(filename=""):
+    """reads file filename and prints it
+    uses with operator
+    filename is the input argument
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data, end="")
