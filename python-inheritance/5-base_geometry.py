5a76e64eb98f18b9f2f83ef61e06a8bf03ffc1ec
Jabulani-N <jabu.94@gmail.com>
2023-05-29 20:08:10 -0700

    task5_created

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module holds a class that inherits class list
+
+it will replicate the funciton of the 'sorted' command
+"""
+
+
+class MyList(list):
+    """does nothing"""
+
+    pass
