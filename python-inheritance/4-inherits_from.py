f6804c018f21980e0481963a5900eaf52d7e12e4
Jabulani-N <jabu.94@gmail.com>
2023-05-29 20:06:54 -0700

    trask4_created

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module holds a function to tell if object is of class"""
+
+
+def inherits_from(obj, a_class):
+    """if it's inherited, but not same type.
+
+    uses type and as isinstance:
+    type does not allow inherited to be True
+    isinstance does
+    """
+    a = type(obj)
+    b = type(a_class)
+    if b == a:
+        return False
+    else:
+        if isinstance(obj, a_class):
+            return True
+    return False  # if it's not the type, and not a child
