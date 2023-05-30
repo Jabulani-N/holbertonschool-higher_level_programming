dd8d748a5ad4d8ff3af9b3dfba659ba1b1616e01
Jabulani-N <jabu.94@gmail.com>
2023-05-30 13:57:09 -0700

    task 0 created. incomplete

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module will read a file and print it"""
+
+
+def read_file(filename=""):
+    """reads file filename and prints it
+    uses with operator
+    """
+
+    def area(self):
+        """will do something"""
+        pass
