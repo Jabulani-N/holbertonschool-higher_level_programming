a201a854d9bf39b1f6bd1021723e52afad7492a0
Jabulani-N <114835664+Jabulani-N@users.noreply.github.com>
2023-05-31 10:55:53 -0500

    Create 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""moldule is script
+takes indeterminate number of input agrs
+add them all to a list
+dump the list to file
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+  data = load_from_json_file('add_item.json')
+except:
+  data = []
+data += sys.argv[1:]
+
+save_to_json_file(data, 'add_item.json')
