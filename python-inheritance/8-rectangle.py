e006a740e1a78675869bae2db785545bb9ca77f4
Jabulani-N <jabu.94@gmail.com>
2023-05-30 21:09:51 -0700

    task8_created

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""imports base_geometry
+elaborates
+leaves
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle (BaseGeometry):
+    """elaboration of base geometry"""
+
+    def __init__(self, width, height):
+        self.integer_validator("",width)
+        self.integer_validator("",height)
+        self.__width = width
+        self.__height = height
