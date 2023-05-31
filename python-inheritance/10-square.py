00d2fafee60c26d7d74cdf55cd2a801e4e0538f9
Jabulani-N <jabu.94@gmail.com>
2023-05-30 21:45:06 -0700

    style_improvements

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -12,10 +12,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square (Rectangle):
     """elaboration of rectangle"""
-
-
     def __init__(self, size):
         super.__init__(size, size)
         self.__size = size
-
-
