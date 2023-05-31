3100350f86ee6bca4198d7c1342e1cb28e06304f
Jabulani-N <114835664+Jabulani-N@users.noreply.github.com>
2023-05-31 09:33:54 -0500

    Update 8-class_to_json.py for coding efficiency
    kept commented code that definately gets full marks for Holberton. It is likely that the __dict__ approach I tried before had syntax errors in it's use, and I did not properly understand the error message thrown to mean that.

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,11 +7,15 @@ requires no import
 
 def class_to_json(obj):
     """returns list of obj's dict"""
-    dict_desc = {}
+    return obj.__dict__
 
-    attributes = obj.__dict__
+# the below got ful marks. I tried the above, but it did not work.
+# I later learned that it was indeed the approach. likely syntax error.
+# dict_desc = {}
 
-    for attr, value in attributes.items():
-        if type(value) in [list, dict, str, int, bool]:
-            dict_desc[attr] = value
-    return dict_desc
+    # attributes = obj.__dict__
+
+    # for attr, value in attributes.items():
+        # if type(value) in [list, dict, str, int, bool]:
+            # dict_desc[attr] = value
+    # return dict_desc
