50abe26b22c029d4b3fb6c8cd9cec865a75d53eb
Jabulani-N <114835664+Jabulani-N@users.noreply.github.com>
2023-05-31 10:57:21 -0500

    style fixes

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,9 +10,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
-  data = load_from_json_file('add_item.json')
+    data = load_from_json_file('add_item.json')
 except:
-  data = []
+    data = []
 data += sys.argv[1:]
 
 save_to_json_file(data, 'add_item.json')
