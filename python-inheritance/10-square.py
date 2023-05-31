34a0bb96ebc3650a632dbb92d653c8b3604ba319
Jabulani-N <jabu.94@gmail.com>
2023-05-30 21:44:36 -0700

    task10_created

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""imports rectangle
+rectangle imports base_gemoetry
+rectangle elaborates
+rectangle leaves
+elaborates
+leaves
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square (Rectangle):
+    """elaboration of rectangle"""
+
+
+    def __init__(self, size):
+        super.__init__(size, size)
+        self.__size = size
+
+
