84df91574b9dd33250c87b9c9b0d2cc4358b3938
Jabulani-N <jabu.94@gmail.com>
2023-05-30 20:44:10 -0700

    task9_updated

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -10,4 +10,12 @@ class Student():
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
+    def to_json(self):
+        """does the same as task8, but to self"""
+        dict_desc = {}
+        attributes = self.__dict__
+        for attr, value in attributes.items():
+            if type(value) in [list, dict, str, int, bool]:
+                dict_desc[attr] = value
+        return dict_desc
