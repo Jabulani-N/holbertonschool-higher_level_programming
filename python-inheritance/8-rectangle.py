fef72c9390aad42b16e23b6b6d55106f39722b36
Jabulani-N <jabu.94@gmail.com>
2023-05-30 21:12:07 -0700

    style_corrections

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,11 +6,11 @@ leaves
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle (BaseGeometry):
     """elaboration of base geometry"""
-
     def __init__(self, width, height):
-        self.integer_validator("width",width)
-        self.integer_validator("height",height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
