78121faff6213fede3a6aaab06f21f233415e04e
Jabulani-N <jabu.94@gmail.com>
2023-05-30 21:53:41 -0700

    syntax_fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -13,5 +13,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square (Rectangle):
     """elaboration of rectangle"""
     def __init__(self, size):
-        super.__init__(size, size)
+        self.integer_validator("size", size)
+        # so it complains correcctly if size is bad
+        super().__init__(size, size)
         self.__size = size
