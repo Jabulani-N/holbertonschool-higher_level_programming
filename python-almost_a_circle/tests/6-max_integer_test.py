908eef1eb1f39aef32e956716f43e95a8d7f0bd3
Jabulani-N <jabu.94@gmail.com>
2023-06-01 15:59:16 -0700

    cleaned_up_unittest_file

--- python-almost_a_circle/tests/6-max_integer_test.py
+++ python-almost_a_circle/tests/6-max_integer_test.py
@@ -5,32 +5,22 @@
 
 import unittest
 
-max_integer = __import__('base').Base
+Base = __import__('base').Base
 
 
 class testingTask1(unittest.TestCase):
-    """tests a number of potential inputs to the provided file"""
+    """tests class for task 1
+    define a test, set up whatever you need between the
+    def and self.assert.
 
+    a proper setup() function will accelerate class creation
+    """
+
+
+    # self.assert(thing, what_thing_should_equal_to_pass_test)
     def test_empty_input(self):
-        self.assertEqual(max_integer([]), None)
-
-    def test_len1_list(self):
-        self.assertEqual(max_integer([3]), 3)
-
-    def test_len3_list(self):
-        self.assertEqual(max_integer([3, 65, 4]), 65)
-
-    def test_with_negatives(self):
-        self.assertEqual(max_integer([-3, -65, -4]), -3)
-
-    def test_ending_max(self):
-        self.assertEqual(max_integer([-3, -9999, -1]), -1)
-
-    def test_middle_max(self):
-        self.assertEqual(max_integer([-3, 9999, -1]), 9999)
-
-    def test_two_max(self):
-        self.assertEqual(max_integer([-3, 1, 1]), 1)
+        newbase = Base(4444)
+        self.assertEqual(newbase.id, 4444)
 
 
 if __name__ == '__main__':
