518caad71cc5f846caf70ca1db6d87bf83a65ce1
Jabulani-N <jabu.94@gmail.com>
2023-06-02 14:54:18 -0700

    introuded_validator-began_area

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,6 +21,19 @@ class Rectangle (Base):
         self.__x = x
         self.__y = y
 
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate value. assume name will be string
+        based on inheritance project task 7
+        assume name is always stirng
+        """
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
     @property
     def width(self):
         """gets above property"""
