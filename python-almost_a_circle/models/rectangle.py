32080c49ba986aedc7f22aefb3e5c602a0de0c45
Jabulani-N <jabu.94@gmail.com>
2023-06-02 15:54:49 -0700

    validator_now_accepts_0_where_appropriate-via_alternative_validator

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,17 @@ from models.base import Base
 class Rectangle (Base):
     """inehrits from base"""
 
-    def integer_validator(self, name, value):
+    def integer_validator0(self, name, value):
+        """validate value. assume name will be string
+        based on python-inheritance project task 7
+        assume name is always stirng
+        """
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value < 0:
+            raise ValueError(name + " must be >= 0")
+
+    def integer_validator1(self, name, value):
         """validate value. assume name will be string
         based on python-inheritance project task 7
         assume name is always stirng
@@ -20,17 +30,16 @@ class Rectangle (Base):
             raise TypeError(name + " must be an integer")
         if value <= 0:
             raise ValueError(name + " must be > 0")
-        return True  # so we have a success output
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """all starter attributes will be private
         all will use getters and setters
         """
         super().__init__(id)
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.integer_validator("x", x)
-        self.integer_validator("y", y)
+        self.integer_validator1("width", width)
+        self.integer_validator1("height", height)
+        self.integer_validator0("x", x)
+        self.integer_validator0("y", y)
         self.__width = width
         self.__height = height
         self.__x = x
@@ -44,7 +53,7 @@ class Rectangle (Base):
     @width.setter
     def width(self, imp):
         """sets above property"""
-        if self.integer_validator("width", imp):
+        if self.integer_validator1("width", imp):
             self.__width = imp
 
     @property
@@ -55,7 +64,7 @@ class Rectangle (Base):
     @height.setter
     def height(self, imp):
         """sets above property"""
-        if self.integer_validator("height", imp):
+        if self.integer_validator1("height", imp):
             self.__height = imp
 
     @property
@@ -66,7 +75,7 @@ class Rectangle (Base):
     @x.setter
     def x(self, imp):
         """sets above property"""
-        if self.integer_validator("x", imp):
+        if self.integer_validator0("x", imp):
             self.__x = imp
 
     @property
@@ -77,5 +86,5 @@ class Rectangle (Base):
     @y.setter
     def y(self, imp):
         """sets above property"""
-        if self.integer_validator("y", imp):
+        if self.integer_validator0("y", imp):
             self.__y = imp
