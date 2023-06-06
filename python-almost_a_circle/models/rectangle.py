906f0af1229743864fee39a6985aaf588006b2fa
Jabulani-N <jabu.94@gmail.com>
2023-06-06 16:22:19 -0700

    updated_rectangle.py-to_didctoinary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -176,4 +176,10 @@ class Rectangle (Base):
 
     def to_dictionary(self):
         """return own dictioniary"""
-        return self.__dict__
+        return {
+            "id":  self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+               }
