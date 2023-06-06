f2b535c4a759a96bebba7690c8bfac47e20c8849
Jabulani-N <jabu.94@gmail.com>
2023-06-06 16:29:12 -0700

    introduced_square_size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -47,3 +47,12 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """return own dictioniary"""
+        return {
+            "id":  self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+               }
