b0bbf1f938078b275042ab557d46126290bd0039
Jabulani-N <jabu.94@gmail.com>
2023-06-06 13:30:48 -0700

    corrected_self_property_collection

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,19 +4,17 @@ import your previous integer validator,
 use it on the setters
 it'll save space from validating every setter manually
 """
-
 from models.rectangle import Rectangle
 
 
-class Square (Rectangle):
+class Square(Rectangle):
     """inehrits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
         """all starter attributes handled by super"""
         super().__init__(size, size, x, y, id)
 
-
     def __str__(self):
         return "[Square] (" + str(self.id) + ") " +\
-               str(self.__x) + "/" + str(self.__y) + " - " +\
-               str(self.__width)
+               str(self.x) + "/" + str(self.y) + " - " +\
+               str(self.width)
