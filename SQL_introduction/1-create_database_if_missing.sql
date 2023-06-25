024725cc20e239f6e79a7e8d77f22cca7b319e3a
Jabulani-N <jabu.94@gmail.com>
2023-06-24 20:32:49 -0700

    cleaned_code

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,2 @@
 -- Creartes a database in the SQL server if it does not already exist
-
 CREATE database IF NOT EXISTS hbtn_0c_0
--- IF NOT EXISTS (hbtn_0c_0)
-    -- in this case, we'll create the database
-   --  {CREATE database hbtn_0c_0}
