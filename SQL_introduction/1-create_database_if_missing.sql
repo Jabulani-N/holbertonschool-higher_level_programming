bc30d1a6596f8f8eff98bf618ec8fb5f6437b1cd
Jabulani-N <jabu.94@gmail.com>
2023-06-24 20:30:32 -0700

    testing_a_syntax_for_cnditional_creation

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,6 @@
 -- Creartes a database in the SQL server if it does not already exist
 
-CREATE database hbtn_0c_0
+CREATE database IF NOT EXISTS hbtn_0c_0
 -- IF NOT EXISTS (hbtn_0c_0)
     -- in this case, we'll create the database
    --  {CREATE database hbtn_0c_0}
