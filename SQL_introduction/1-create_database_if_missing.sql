4cf1f6f9bc3797e5905faa642f0591966da3ff00
Jabulani-N <jabu.94@gmail.com>
2023-06-24 20:11:51 -0700

    task1_syntax_fix

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- Creartes a database in the SQL server if it does not already exist
-IF DB_ID('hbtn_0c_0') IS NOT NULL
+IF NOT EXISTS (hbtn_0c_0)
     -- in this case, we'll create the database
-    CREATE database hbtn_0c_0
+    {CREATE database hbtn_0c_0}
