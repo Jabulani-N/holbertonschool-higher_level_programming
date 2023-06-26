992378c46f4dc8b6f00340c6cafb718f72781f16
Jabulani-N <jabu.94@gmail.com>
2023-06-25 19:58:56 -0700

    taskk6_updated

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,2 +1,3 @@
--- describes the table first_table in database named in input argument
-SHOW CREATE TABLE first_table
+-- script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
+-- database name will be passed as an argument of the mysql command
+SELECT * first_table
