f7201f2e07a2361ee9726834d8e39fc219941dba
Jabulani-N <jabu.94@gmail.com>
2023-06-25 21:18:25 -0700

    task10 commentary corrected

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,5 @@
 -- script that lists all records of the table second_table
--- every score with every name associated with it.
+-- each name's top score, and the name to the right of it
+-- name as well.
 -- in order of highest score at top; lowest at bottom.
 SELECT score, name FROM second_table ORDER BY score DESC
