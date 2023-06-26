764f6149b05c985cd30d4fd534ef570c582bd32e
Jabulani-N <jabu.94@gmail.com>
2023-06-25 21:06:07 -0700

    task10 created

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- script that lists all records of the table second_table
+-- each name's top score, and the name to the right of it
+-- name as well.
+-- in order of highest score at top; lowest at bottom.
+SELECT score, name FROM second_table ORDER BY
