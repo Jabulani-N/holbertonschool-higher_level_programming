db735b7fc5a3bface470b043a412bca3e432bb16
Jabulani-N <jabu.94@gmail.com>
2023-06-25 22:50:11 -0700

    task16_syntax_fix

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -5,6 +5,5 @@
 SELECT score, name
 FROM second_table
 WHERE EXISTS
-(SELECT name FROM second_table)
-WHERE name IS NOT NULL
+(SELECT name FROM second_table) AND name IS NOT NULL
 ORDER BY score DESC;
