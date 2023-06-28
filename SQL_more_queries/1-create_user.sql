4f0d28ab3177af2f6db23746037817d798870020
Jabulani-N <jabu.94@gmail.com>
2023-06-27 17:11:08 -0700

    task1 added newline at End of File

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,3 +4,7 @@
 -- If the user user_0d_1 already exists, your script should not fail
 CREATE USER IF NOT exists 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
