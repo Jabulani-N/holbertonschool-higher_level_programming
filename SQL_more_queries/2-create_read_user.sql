42cb36d0a2eafc7a1da772bb82355aa0f265c1da
Jabulani-N <jabu.94@gmail.com>
2023-06-27 20:24:40 -0700

    task2_created

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2
+-- only give it SELECT privilege in the database hbtn_0d_2
+-- password user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT exists 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
+REVOKE ALL PRIVILEGES ON hbtn_0d_2.* FROM 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
