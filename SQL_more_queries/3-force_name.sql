dba3b205517547e48e014d4a22b2641377309ebc
Jabulani-N <jabu.94@gmail.com>
2023-06-30 17:14:22 -0700

    task3 added a note as to why it likely passed checks

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
-
+--this likely worked because of the table I made in previous versions of this code haivng unwanted constraints
