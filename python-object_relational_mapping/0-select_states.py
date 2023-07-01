162879eb33dd9ac6135b7ec0e2a8c9950789aec4
Jabulani-N <jabu.94@gmail.com>
2023-07-01 13:34:16 -0700

    task0 begun

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
-"""moldue with a script that lists all states from the database hbtn_0e_0_usa"""
+"""moldue with a script that lists all states from the database hbtn_0e_0_usa
+does not activate when imported
 
+script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+"""
+
+def list_all_states (username, password, dbname):
+{
+}
+
+
+
+if __name__ == '__main__':
+    list_all_states()
