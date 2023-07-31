0dab0d7ce8e3d08984657a48352103bce0afa010
Jabulani-N <jabu.94@gmail.com>
2023-07-31 15:25:55 -0700

    semistandard style improvements

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ const args = require('process').argv;
 require('process').argv returns an array
 [javascript location (shebang), called function/file, user arguments]
 */
-if (typeof args[2] == 'undefined'  ) {
+if (typeof args[2] === 'undefined') {
   console.log('No argument');
 } else {
   console.log(args[2]);
