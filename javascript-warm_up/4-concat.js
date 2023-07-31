4df201e0bed130fa03816fe8a6a2971502aca0e9
Jabulani-N <jabu.94@gmail.com>
2023-07-31 16:03:39 -0700

    task4 semistandard style improvements

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 // this code is intentionally inefficnet,
-//for the sake of creating examples to reference
+// for the sake of creating examples to reference
 const argv = require('process').argv;
-let first = 'undefined'
-let second = 'undefined'
+let first = 'undefined';
+let second = 'undefined';
 
-if (typeof argv[2] != 'undefined'){
+if (typeof argv[2] !== 'undefined') {
   first = argv[2];
 }
-if (typeof argv[3] != 'undefined'){
+if (typeof argv[3] !== 'undefined') {
   second = argv[3];
 }
-const finalAnswer = first.concat(' is ',second )
+const finalAnswer = first.concat(' is ', second);
 
-  console.log(finalAnswer);
+console.log(finalAnswer);
