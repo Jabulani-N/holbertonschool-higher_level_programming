9d60df53ebf94464964a7c06471d569bd3c75c29
Jabulani-N <jabu.94@gmail.com>
2023-08-07 21:57:45 -0700

    task4_completes_requested_task

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -8,14 +8,24 @@ require('process').argv returns an array:
 const listOfFilms = args[2];
 
 const request = require('request');
-
+let timesFeatured = 0
 request.get(listOfFilms, function (error, response) {
   if (error) {
     console.log(error.message);
   } else {
     // we received the list of films
     const objectified = JSON.parse(response.body);
-    console.log(objectified.results);
+    // console.log(objectified.results);
+    // objectiied.results is a list of film objects
+    for (const filmObject of objectified.results) {
+      for (const characterURL of filmObject.characters) {
+        // console.log(characterURL)
+        if (characterURL == 'https://swapi-api.hbtn.io/api/people/18/') {
+            timesFeatured++;
+        }
+      }
+    }
+    console.log(timesFeatured.toString())
   }
 }
 );
